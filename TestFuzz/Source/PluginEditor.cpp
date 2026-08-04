48c5e1dd0b95ca8f269ce6fbff93d2bd24131a85
ip0627 <ip0627@yahoo.co.jp>
2026-08-04 14:12:38 +0900

    再度JUCEプロジェクトを作成。

---
+++ TestFuzz/Source/PluginEditor.cpp
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+TestFuzzAudioProcessorEditor::TestFuzzAudioProcessorEditor (TestFuzzAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+TestFuzzAudioProcessorEditor::~TestFuzzAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void TestFuzzAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (juce::FontOptions (15.0f));
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void TestFuzzAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
